8e0cad7a8a4c83b79ffa8d68762648356fc1fd49
ipdandra <ipdandra@IP-NB64>
2024-07-29 07:40:09 +0200

    Dodaj pliki projektów.

---
+++ SygnalistaNF/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+namespace SygnalistaNF
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnSendButtonClicked(object sender, EventArgs e)
+        {
+            // Obsługa kliknięcia przycisku
+            string messageText = TextInput.Text;
+            string selectedName = NamePicker.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedName))
+            {
+                await DisplayAlert("Błąd", "Proszę wybrać imię z listy.", "OK");
+                return;
+            }
+
+
+
+
+
+            // Tworzenie wiadomości e-mail
+             var message = new MimeMessage();
+             message.From.Add(new MailboxAddress("Syngalista", "sygnalista.gal.anonim@gmail.com"));
+             message.To.Add(new MailboxAddress("Odbiorca", "krzysztof.blicharz@infopower.pl"));
+             message.Subject = "Donos";
+             message.Body = new TextPart("plain")
+             {
+                 Text = $"Cześć, {selectedName} {messageText}"
+             };
+
+            // Konfiguracja klienta SMTP using (var client = new SmtpClient())
+            using (var client = new SmtpClient())
+            {
+                // Configure client to ignore SSL certificate validation errors
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                try
+                {
+                    await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                    await client.AuthenticateAsync("sygnalista.gal.anonim@gmail.com", "bccm oogr uzct jtyi");
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                     await DisplayAlert("Wiadomość", "E-mail został wysłany!", "OK");
+
+                    // Czyszczenie pola tekstowego
+                    TextInput.Text = string.Empty;
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Błąd", $"Wysłanie e-maila nie powiodło się: {ex.Message}", "OK");
+                }
+            }
+        
+
+        }
+
+    }
+}
