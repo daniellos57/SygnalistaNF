1e5b17661b9432f5f2c9685fea5c2010bae97438
daniellos57 <daniel_drapiewski@wp.pl>
2024-07-29 07:49:26 +0200

    Changes

---
+++ SygnalistaNF/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SygnalistaNF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SygnalistaNF/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SygnalistaNF
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
